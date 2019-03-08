ee6ac95617ca43a372538c9c7c1ddef0b34ea7c2
Kimi Liang <kimi.liang@accentrix.com>
2019-03-08 17:04:05 +0800

    First Commit

--- src/main.js
+++ src/main.js
@@ -3,10 +3,38 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './errorLog' // error log
+import './permission' // permission control
+import './mock' // simulation data
+
+import * as filters from './filters' // global filters
+
 Vue.config.productionTip = false
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: '/',
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    // 必须清除已有的所有 loader，否则接下来的loader会附加在该规则现有的loader 之后。
+    svgRule.uses.clear()
+    // 添加要替换的 loader
+    svgRule
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+  }
+}
