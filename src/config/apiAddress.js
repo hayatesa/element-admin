e7d5968dfd8ce866d0b8a5799fb648ac769b6566
Kimi Liang <kimi.liang@accentrix.com>
2019-04-02 18:09:22 +0800

    Update api address

--- src/config/apiAddress.js
+++ src/config/apiAddress.js
@@ -1,4 +1,4 @@
 module.exports = {
-  AUTH_ADDRESS: 'http://127.0.0.1:5243/auth/api',
-  SYS_ADDRESS: 'http://127.0.0.1:5243/sys/api'
+  AUTH_ADDRESS: 'http://127.0.0.1:5001/api',
+  SYS_ADDRESS: 'http://127.0.0.1:5000/api'
 }
